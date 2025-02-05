7ccf9a624a66e785256fd482d0fcddc908d8ef85
Andrew Perotti <Perotti28@gmail.com>
2025-02-05 09:57:05 -0800

    Omit outdated logic, add new listener logic
    Earlier versions called the htmlContent modules function that generated content on the page, however we needed to set up listeners for buttons to generate content when they are clicked so I've omitted the previous code that generated buttons and home content and now use delegation on the nav to set up multiple listeners on the buttons which will invoke functions from modules to generate content for those sections.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
-import content from "./htmlContent.js" 
-content()
+import homeContent from "./homeContent.js" 
+
+let nav = document.querySelector('nav')
+let divContent = document.querySelector('div#content')
+
+homeContent()
+
+nav.addEventListener('click', e=>{
+    if(e.target.id==='home'){
+        divContent.textContent=''
+        homeContent()
+    }else if(e.target.id==='menu'){
+        divContent.textContent=''
+    }else if(e.target.id==='contact'){
+        divContent.textContent=''
+    }
+})
